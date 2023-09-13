34ea64b65948078cb2fa253d15ec75ffa9627e15
Gabriel Soares da Silva Góes <gabriel_soares1125@outlook.com>
2023-09-13 14:59:00 -0300

    Update confluenceContributors.py
    Complete restructure of the code: 
    - Reordered functions for better readability; 
    - Added a config file for sensitive data

--- confluenceContributors.py
+++ confluenceContributors.py
@@ -4,127 +4,136 @@ from datetime import datetime
 import requests
 from requests.auth import HTTPBasicAuth
 import pandas as pd
+import json
 
+def main():
+    global AUTH, HEADERS, configData
+    configData = getDataFromJson()
+    AUTH, HEADERS = setRequestsData()
+    getUserPagesAmount()
 
-auth = HTTPBasicAuth("email", "token")
+def getDataFromJson():
+    with open('configData.json', 'r') as arquivoJson:
+        configData = json.load(arquivoJson)
+    return configData
 
-headers = {
-    'Accept': 'application/json'
-}
+def setRequestsData():
+    auth = HTTPBasicAuth(configData["Email"], configData["APIToken"])
+    headers = {
+        'Accept': 'application/json'
+    }
+    return auth, headers
+
+def getUserPagesAmount():
+    pageIDs = getPagesID()
+    userNamesList = []
+
+    for pageID in pageIDs:
+        userIDs = getUsersID(pageID)
+        userNames = [getUserName(userID) for userID in userIDs]
+        userNamesList.extend(userNames)
+
+    contagemPorPessoa = contarOcorrencias(userNamesList)
+    dataFrame = pd.DataFrame(contagemPorPessoa.items(), columns=['Analista', 'Total Criado Neste Ano'])
+    dataFrameOrdenado = dataFrame.sort_values(by='Total Criado Neste Ano', ascending=False)
+    exportacaoXLSX(dataFrameOrdenado)
+    return escreverPaginaConfluence(dataFrameOrdenado)
+
+def getPagesID():
+    baseURL = f'https://{configData["CompanyDomain"]}.atlassian.net/wiki/api/v2/spaces/'
+    params = {
+        'cql': f"created >= startOfYear()",
+        'limit': 250,
+        'start': 0
+    }
+    
+    response = requests.get(baseURL, headers=HEADERS, params=params, auth=AUTH)
+    data = response.json()
+    pageID = [page['id'] for page in data.get('results', [])]
+    return pageID
+
+def getUsersID(ID):
+    baseURL = f'https://{configData["CompanyDomain"]}.atlassian.net/wiki/api/v2/spaces/{ID}/pages'
+    params = {
+        'cql': f"created >= startOfYear()",
+        'limit': 250,
+        'start': 0
+    }
+    response = requests.get(baseURL, headers=HEADERS, auth=AUTH, params=params)
+    data = response.json()
+    userID = filterYear(data)
+    return userID
 
 def filterYear(data):
-    this_year = dt.datetime.now().year
-    users_this_year = [
+    thisYear = dt.datetime.now().year
+    usersThisYear = [
         page['authorId']
         for page in data['results']
-        if dt.datetime.fromisoformat(page['createdAt'].replace('Z', '+00:00')).year == this_year
+        if dt.datetime.fromisoformat(page['createdAt'].replace('Z', '+00:00')).year == thisYear
     ]
-    return users_this_year
-
-def contar_ocorrencias(lista):
-    return Counter(lista)
+    return usersThisYear
 
 def getUserName(UserID, cache={}):
     if UserID in cache:
         return cache[UserID]
 
-    base_url = f'https://your-space.atlassian.net/wiki/rest/api/user?accountId={UserID}'
+    baseURL = f'https://{configData["CompanyDomain"]}.atlassian.net/wiki/rest/api/user?accountId={UserID}'
     params = {
         'cql': f"created >= startOfYear()",
         'limit': 250,
         'start': 0
     }
-    try:
-        response = requests.get(base_url, headers=headers, auth=auth, params=params)
-        data = response.json()
-        if 'publicName' in data:
-            nomeUsuario = data['publicName']
-        else:
-            nomeUsuario = 'Usuário Desconhecido'
-        cache[UserID] = nomeUsuario
-        return nomeUsuario
-    except Exception as e:
-        return f'Erro ao buscar informações do usuário: {e}'
-
-def getUsersID(ID):
-    base_url = f'https://your-space.atlassian.net/wiki/api/v2/spaces/{ID}/pages'
-    params = {
-        'cql': f"created >= startOfYear()",
-        'limit': 250,
-        'start': 0
-    }
-    response = requests.get(base_url, headers=headers, auth=auth, params=params)
-    data = response.json()
-    user_ID = filterYear(data)
-    return user_ID
-
-def getPagesID():
-    base_url = 'https://your-space.atlassian.net/wiki/api/v2/spaces/'
-    params = {
-        'cql': f"created >= startOfYear()",
-        'limit': 250,
-        'start': 0
-    }
-    try:
-        response = requests.get(base_url, headers=headers, params=params, auth=auth)
-        data = response.json()
-        page_ID = [page['id'] for page in data.get('results', [])]
-        return page_ID
-    except Exception as e:
-        print(f"Error fetching page IDs: {e}")
-        return []
-
-def getUserPagesAmount():
-    try:
-        pageIDs = getPagesID()
-        userNamesList = []
-
-        for page_id in pageIDs:
-            userIDs = getUsersID(page_id)
-            userNames = [getUserName(user_id) for user_id in userIDs]
-            userNamesList.extend(userNames)
-
-        contagem_por_pessoa = contar_ocorrencias(userNamesList)
-        df = pd.DataFrame(contagem_por_pessoa.items(), columns=['Analista', 'Total Criado 2023'])
-        df_ordenado = df.sort_values(by='Total Criado 2023', ascending=False)
-        return escreverPaginaConfluence(df_ordenado)
-    except Exception as e:
-        print(f"Error processing user pages amount: {e}")
-        return None
     
-def padrao_de_colunas(worksheet):
+    response = requests.get(baseURL, headers=HEADERS, auth=AUTH, params=params)
+    data = response.json()
+    if 'publicName' in data:
+        nomeUsuario = data['publicName']
+    else:
+        nomeUsuario = 'Usuário Desconhecido'
+    cache[UserID] = nomeUsuario
+    return nomeUsuario
+
+def contarOcorrencias(lista):
+    return Counter(lista)
+
+def exportacaoXLSX(dataFrame):
+    dataAtual = datetime.now()
+    diaAtual = dataAtual.strftime('%d')
+    mesAtual = dataAtual.strftime("%m")
+    anoAtual = dataAtual.year
+    
+    writer = pd.ExcelWriter(f'{configData["PathToFile"]}_{diaAtual}-{mesAtual}-{anoAtual}.xlsx', engine='openpyxl')
+    dataFrame.to_excel(writer, sheet_name='Sheet')
+    workbook = writer.book
+    worksheet = workbook['Sheet']
+    padraoDeColunas(worksheet)
+    writer._save()
+
+def padraoDeColunas(worksheet):
     worksheet.column_dimensions['A'].width = 35
     worksheet.column_dimensions['B'].width = 35
 
-def exportacao_xlsx(data_frames):
-    writer = pd.ExcelWriter("INSERT THE ABSOLUTE PATH FOR THE FILE", engine='openpyxl')
-    data_frames.to_excel(writer, sheet_name='Sheet')
-    workbook = writer.book
-    worksheet = workbook['Sheet']
-    padrao_de_colunas(worksheet)
-    writer._save()
-
 def escreverPaginaConfluence(df):
-    table_html = df.to_html(index=False)
-    url = f"https://your-space.atlassian.net/wiki/rest/api/content/INSERTPAGEIDHERE"
-    response = requests.get(url, headers=headers, auth=auth)
+    tableHTML = df.to_html(index=False)
+    url = f'https://{configData["CompanyDomain"]}.atlassian.net/wiki/rest/api/content/{configData["PageID"]}' 
+    response = requests.get(url, headers=HEADERS, auth=AUTH)
     dados = response.json()
     versaoPage = dados['version']['number']
 
-    update_data = {
+    updateData = {
         'version': {
             'number': versaoPage + 1
         },
-        'title': '[INSERT YOUR PAGE TITLE]',
+        'title': f'{configData["PageTitle"]}',
         'type': 'page',
         'body': {
             'storage': {
-                'value': f'<html><body>{table_html}</body></html>',
+                'value': f'<html><body>{tableHTML}</body></html>',
                 'representation': 'storage'
             }
         }
     }
-    print(requests.put(url, json=update_data, auth=auth, headers=headers))
+    requests.put(url, json=updateData, auth=AUTH, headers=HEADERS)
 
-getUserPagesAmount()
-
+if __name__ == '__main__':
+    main()
